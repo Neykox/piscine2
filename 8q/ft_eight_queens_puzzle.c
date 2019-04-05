aa46fa5b247d19a71db3cde9cb8c24892658f358
LR Anthony <anthonyleroy2017@gmail.com>
2019-04-05 14:01:46 +0200

    a

--- 8q/ft_eight_queens_puzzle.c
+++ 8q/ft_eight_queens_puzzle.c
@@ -2,12 +2,33 @@ int check(int grille[8][8], position, compt)
 {
 	int lig;
 	int col;
+	int k;
 
 	lig = position/8;
 	col = position%8;
+	k = 1;
 	if (grille[i][j] != 0)
         return estValide(grille, position+1);
-    
+    while (k < 8)
+    {
+    	if (ligne(k, grille, lig) && column(k, grille, col) && diago(k, grille, lig, col))
+    	{
+    		grille[lig][col] = k;
+    		if ()
+    	}
+    }
+
+
+
+
+
+        if (absentSurLigne(k,grille,i) && absentSurColonne(k,grille,j) && absentSurBloc(k,grille,i,j))
+        {
+            grille[i][j] = k;
+
+            if ( estValide (grille, position+1) )
+                return true;
+        }
 }
 
 int ft_eight_queens_puzzle(void)
