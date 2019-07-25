1ab8f975a7dfa30c0d358d6d1cab6184a7de7081
Neykox <anthonyleroy2017@gmail.com>
2019-07-25 11:42:01 +0200

    e

--- 8q/ft_eight_queens_puzzle.c
+++ 8q/ft_eight_queens_puzzle.c
@@ -1,14 +1,43 @@
-#include <unistd.h>
+#include <stdio.h>
 
-void ft_putchar(char c)
+int row(int grille [8][8], int row)
 {
-	write(1, &c, 1);
+	int j;
+
+	j = 0;
+	while (j < 8)
+	{
+		if (grille[row][j] != 0)
+			return 0;
+		j++;
+	}
+	return 1;
 }
 
-int check(int grille [8][8], int position, int compt)
+int column(int grille [8][8], int col)
 {
+	int i;
 
+	i = 0;
+	while (i < 8)
+	{
+		if (grille[i][col] != 0)
+			return 0;
+		i++;
+	}
+	return 1;
 }
+
+int check(int grille [8][8], int col, int *compt)
+{
+	if (row(grille, 0) && columnuyè       (grille, 0) /*&& diago(grille, 0)*/)
+	{
+		grille[col][col] = 1;
+	}
+	*compt = *compt + 1;
+	return 0;
+}
+
 
 int ft_eight_queens_puzzle(void)
 {
@@ -21,13 +50,14 @@ int ft_eight_queens_puzzle(void)
 		{0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0},                     
 		{0, 0, 0, 0, 0, 0, 0, 0}
 	};
 
 	compt = 0;
-	check(grille, 0, compt);
-	ft_putchar (compt);
+	check (grille, 0, &compt);
+	printf("%d\n", compt);
+	return 0;
 }
 
 int main()
@@ -35,3 +65,21 @@ int main()
 	ft_eight_queens_puzzle();
 	return 0;
 }
+
+
+// int triple(int *compt)
+// {
+//   *compt *= 3;
+//   return 0;
+// }
+
+// int main()
+// {
+//     int *compt;
+
+//     compt = 42;
+//     triple(&compt);
+//     printf("%d\n", compt);
+//     // ft_eight_queens_puzzle();
+//     return 0;
+// }
---
+++ day05/ex19.c
@@ -0,0 +1,11 @@
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	
+}
+
+int main()
+{
+	printf("Expected: strlcpy() \n");
+	printf("Result: ft_strlcpy() \n");
+	return 0;
+}
